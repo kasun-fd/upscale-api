7e190e7989da4eccf18b546a1e4d3b196c4f649d
Kasun Fernando <fernandokasun672@gmail.com>
2024-06-22 15:39:13 +0530

    image controller updated

--- src/main/java/com/devstack/ecom/upscale/api/ProductImageController.java
+++ src/main/java/com/devstack/ecom/upscale/api/ProductImageController.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
 @RestController
 @RequestMapping("/api/v1/product_image")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductImageController {
 
     private final ProductImageService imageService;
