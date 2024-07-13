708bb94f71950bb433162128908d10f45f17bcb6
Kasun Fernando <fernandokasun672@gmail.com>
2024-07-13 14:37:59 +0530

    security part 2nd commit

---
+++ src/main/java/com/devstack/ecom/upscale/jwt/JwtConfig.java
@@ -0,0 +1,45 @@
+package com.devstack.ecom.upscale.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public JwtConfig() {
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public Integer getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(Integer tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public void setTokenPrefix(String tokenPrefix) {
+        this.tokenPrefix = tokenPrefix;
+    }
+
+    public String getAuthorizationHeader(){
+        return HttpHeaders.AUTHORIZATION;
+    }
+
+}
